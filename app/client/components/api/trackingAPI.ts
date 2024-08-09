633af89bffce07791fd0c41dce9e5d310060372e
suhas-sunder <suhas@live.ca>
2024-08-08 23:14:10 -0400

    fix: disable csp and api error logging

--- app/client/components/api/trackingAPI.ts
+++ app/client/components/api/trackingAPI.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 // Determine the base URL based on environment variables
 const baseURL =
   import.meta.env.MODE === "production"
-    ? `http://localhost:${import.meta.env.VITE_PORT || 5173}/v1/api/tracking`
+    ? `http://localhost:${
+        import.meta.env.VITE_API_PORT || 5173
+      }/v1/api/tracking`
     : `http://localhost:${import.meta.env.VITE_PORT || 3200}/v1/api/tracking`;
 
 console.log("Base URL:", baseURL); // Log base URL for debugging
@@ -12,5 +14,19 @@ const instance = axios.create({
   baseURL,
   timeout: 30000,
 });
+
+// Add request and response interceptors for additional logging
+instance.interceptors.request.use((config) => {
+  console.log("Request URL:", config.url);
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Network Error:", error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
