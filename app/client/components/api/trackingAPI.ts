fdc8b6123fd7bd0051702f238c4210b808c87e14
suhas-sunder <suhas@live.ca>
2024-08-09 03:16:38 -0400

    refactor: error logging on server route.

--- app/client/components/api/trackingAPI.ts
+++ app/client/components/api/trackingAPI.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Determine the base URL based on environment variables
 const baseURL =
   import.meta.env.MODE === "production"
-    ? `/v1/api/tracking`
+    ? `https://emojikitchengame.com/v1/api/tracking`
     : `http://localhost:${import.meta.env.VITE_PORT || 3200}/v1/api/tracking`;
 
 console.log("Base URL:", baseURL); // Log base URL for debugging
