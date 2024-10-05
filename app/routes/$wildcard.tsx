2c09b4ab20f81f97fda3ea2df62cab475f69efef
Suhas Sunder <suhas@live.ca>
2024-10-05 00:40:50 -0400

    refactor: wildcard
    -Update wildcard route for better 404 error handeling.

---
+++ app/routes/$wildcard.tsx
@@ -0,0 +1,42 @@
+import { json, MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: "404 Page Not Found! 🥺",
+    },
+    {
+      name: "description",
+      content:
+        "Oops! The page you are looking for doesn't exist. Please check the URL or return to the homepage! 🎉📲",
+    },
+  ];
+};
+export function loader() {
+  // This will handle the request and return a 404 response
+  return json({ error: "Not Found" }, { status: 404 });
+}
+
+export const action = () => {
+  return new Response("Not Found", { status: 404 });
+};
+
+export default function ErrorBoundary() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+      <h1 className="text-6xl font-bold text-rose-500">🥺 Uh Oh, 404</h1>
+      <h2 className="mt-4 text-2xl font-semibold text-gray-800">
+        Page Not Found...
+      </h2>
+      <p className="mt-2 text-gray-600">
+        The page you are looking for does not exist.
+      </p>
+      <a
+        href="/"
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+      >
+        Go Back Home
+      </a>
+    </div>
+  );
+}
