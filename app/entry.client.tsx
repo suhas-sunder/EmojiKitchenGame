b8508951707148eca4ad4782acbc6e5a7a2f6d23
Suhas Sunder <suhas@live.ca>
2024-10-05 21:11:10 -0400

    feat: posthog analytics
    -Add posthog analytics init to entry client.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,14 +5,28 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+// eslint-disable-next-line import/no-named-as-default
+import posthog from "posthog-js";
+
+function PosthogInit() {
+  useEffect(() => {
+    posthog.init("phc_mLbknZ7mYfJxODEdfCwb8T2Rmc6S488fHIiU9TARqWM", {
+      api_host: "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    });
+  }, []);
+
+  return null;
+}
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
       <RemixBrowser />
+      <PosthogInit />
     </StrictMode>
   );
 });
