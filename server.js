bf922c1566b0b89c358ed21383d803d2b8c87cce
Suhas Sunder <suhas@live.ca>
2024-10-16 13:24:55 -0400

    refactor: server cors

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.use(
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:5174",
       "https://emojikitchengame.com",
       "https://www.emojikitchengame.com",
       "emojikitchengame.com",
